b4c9e74319639976abe28f99c3dc31352457699f
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-24 17:17:49 -0500

    Update boarding_music.py

--- scripts/boarding_music.py
+++ scripts/boarding_music.py
@@ -1,1 +1,13 @@
+import os
+from pygame import mixer
+
+AUDIO_DIR = '/home/pi/PCASS/audio/'
+
+def play_boarding_music():
+    mixer.init()
+    mixer.music.load(os.path.join(AUDIO_DIR, 'BoardingMusic.mp3'))
+    mixer.music.play()
+
+if __name__ == "__main__":
+    play_boarding_music()
 
