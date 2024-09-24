425055b0d0954d914f3ac4ad7261c76d5c868283
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-24 17:17:35 -0500

    Update cabin_alert.py

--- scripts/cabin_alert.py
+++ scripts/cabin_alert.py
@@ -1,1 +1,13 @@
+import os
+from pygame import mixer
+
+AUDIO_DIR = '/home/pi/PCASS/audio/'
+
+def play_cabin_alert():
+    mixer.init()
+    mixer.music.load(os.path.join(AUDIO_DIR, 'CabinAlert.mp3'))
+    mixer.music.play()
+
+if __name__ == "__main__":
+    play_cabin_alert()
 
