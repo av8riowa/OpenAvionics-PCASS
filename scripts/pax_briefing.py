4daa09771f4fdef8c856a4c42269bb92da5380af
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-24 17:18:07 -0500

    Update pax_briefing.py

--- scripts/pax_briefing.py
+++ scripts/pax_briefing.py
@@ -1,1 +1,13 @@
+import os
+from pygame import mixer
+
+AUDIO_DIR = '/home/pi/PCASS/audio/'
+
+def play_pax_briefing():
+    mixer.init()
+    mixer.music.load(os.path.join(AUDIO_DIR, 'PaxBrief.mp3'))
+    mixer.music.play()
+
+if __name__ == "__main__":
+    play_pax_briefing()
 
