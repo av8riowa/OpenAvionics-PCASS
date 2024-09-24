ce83a5d5ff7cb34eeebea88c5b35ffe8b90aa715
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-24 17:17:24 -0500

    Update seatbelt_sign.py

--- scripts/seatbelt_sign.py
+++ scripts/seatbelt_sign.py
@@ -1,1 +1,13 @@
+import os
+from pygame import mixer
+
+AUDIO_DIR = '/home/pi/PCASS/audio/'
+
+def play_seatbelt_sign():
+    mixer.init()
+    mixer.music.load(os.path.join(AUDIO_DIR, 'SBsign.mp3'))
+    mixer.music.play()
+
+if __name__ == "__main__":
+    play_seatbelt_sign()
 
