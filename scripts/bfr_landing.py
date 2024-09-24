4aadc2c892d8c662d75447c73896b17bcf5bdcad
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-24 17:18:24 -0500

    Update bfr_landing.py

--- scripts/bfr_landing.py
+++ scripts/bfr_landing.py
@@ -1,1 +1,13 @@
+import os
+from pygame import mixer
+
+AUDIO_DIR = '/home/pi/PCASS/audio/'
+
+def play_bfr_landing():
+    mixer.init()
+    mixer.music.load(os.path.join(AUDIO_DIR, 'BfrLand.mp3'))
+    mixer.music.play()
+
+if __name__ == "__main__":
+    play_bfr_landing()
 
