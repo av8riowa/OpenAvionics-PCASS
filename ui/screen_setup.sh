64e1436c660800a62b99528d5d57fbac4a079ddb
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-24 17:14:51 -0500

    Update screen_setup.sh

--- ui/screen_setup.sh
+++ ui/screen_setup.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+# Install dependencies for touchscreen
+sudo apt update
+sudo apt install -y xserver-xorg xinit xserver-xorg-video-fbdev
+
+# Download the appropriate drivers for the WaveShare 3.5-inch touchscreen
+git clone https://github.com/waveshare/LCD-show.git
+cd LCD-show/
+
+# Set up the 3.5-inch screen driver
+sudo ./LCD35-show
+
+# After installation, reboot the system
+sudo reboot
 
